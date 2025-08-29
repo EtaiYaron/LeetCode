a98b73053e1eb13d2c8f9bbfc8ba7457f3ae1309
Etai <yaronet@post.bgu.ac.il>
2025-08-29 11:40:20 +0300

    Squashed 'LeetCode-JumpGame/' content from commit a65f62c
    git-subtree-dir: LeetCode-JumpGame
    git-subtree-split: a65f62cddbc94e8bb72a01dc6a2f2ec158f3222a

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public bool CanJump(int[] nums)
+    {
+        int[] memo = new int[nums.Length];
+        for (int i = 0; i < memo.Length; i++)
+            memo[i] = -1;
+        return CanJump(nums, 0, memo);
+    }
+
+    public bool CanJump(int[] nums, int i, int[] memo)
+    {
+        if (i >= nums.Length - 1)
+            return true;
+        if (i < 0 || i >= nums.Length)
+            return false;
+        if (memo[i] != -1)
+            return (memo[i] > 0);
+        int jump = nums[i];
+        if (jump == 0)
+            return false;
+        bool ans = false;
+        for (int j = 1; j <= jump && !ans; j++)
+        {
+            ans = CanJump(nums, i + j, memo);
+        }
+        if (ans)
+            memo[i] = 1;
+        else
+            memo[i] = 0;
+        return ans;
+    }
+}
